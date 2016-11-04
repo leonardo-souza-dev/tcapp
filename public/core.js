59b334e61a54f838e2a69639e2a05773863ad08c
Leonardo <leonardotreze@gmail.com>
2016-11-04 02:51:09 -0200

    angular update

--- public/core.js
+++ public/core.js
@@ -1,253 +1,251 @@
-// public/core.js
-var tcapp = angular.module('tcapp', []);
-var estiloTarefaConcluida = {
-			'background-color': '#CCCCCC',
-			'color': '#EEEEEE !important',
-			'text-decoration': 'line-through'
-		};
-var estiloTarefaNaoConcluida = {
-			'background-color': '#FFF',
-			'color': 'rgba(0,0,0,0.87)',
-			'text-decoration': 'none'
-		};
+(function() {
 
-function formatarDDMMYYYY(dateFieldValue){
+	var tcapp = angular.module('tcapp', ['ngRoute']);
 
-	var year = dateFieldValue.getFullYear()+"";
-	var month = (dateFieldValue.getMonth()+1)+"";
-	var day = dateFieldValue.getDate()+"";
-	var dateFormat = day + "/" + month + "/" + year;
-
-	return dateFormat;
-}
-
-function formatarDDMMYYYYHHMM(data){
-
-	var dateFieldValue = new Date();
-	var dd = dateFieldValue.getDate() + "";
-	var mo = (dateFieldValue.getMonth() + 1) + "";
-	var yyyy = dateFieldValue.getFullYear() + "";
-	var hh = dateFieldValue.getHours() + "";
-	var mm = dateFieldValue.getMinutes() + "";
-	var dateFormat = dd + "/" + mo + "/" + yyyy + " " + hh + "h" + mm;
-
-	return dateFormat;
-}
-
-function obterTarefas($scope, $http){
 	
-	$http.get('/api/obterTarefas').success(function(data){
-
-		//console.log('data');console.log(data);console.log('');
-
-		//$scope.tarefas = data.objeto.tarefas;
-	}).error(function(data){
-        console.log('');
-	});
-}
-
-function addDays(date, days) {
-    var result = new Date(date);
-    result.setDate(result.getDate() + days);
-    return result;
-}
-
-function obterTarefass(d){
-
-	var tarefas = [];
-	var tarefasDb = d;
-
-	var qtd = tarefasDb.length;
-
-	for(i = 0; i < qtd; i++){
-		if (tarefasDb[i].concluida == true) {
-			tarefasDb[i].estilo = estiloTarefaConcluida;
-		} else if (tarefasDb[i].concluida == false) {
-			tarefasDb[i].estilo = estiloTarefaNaoConcluida;
-		} 
-		tarefas.push(tarefasDb[i]);
-	}
-
-	return tarefas;
-}
-
-function obterTarefasDoDia($scope, $http){
-
-	$scope.tarefasDoDia = $http.post('/api/obterTarefas', { dia: diaInicial }).then( function successCallback(response){
+	tcapp.config(function($routeProvider){
 		
-		console.log(response.data.objeto.tarefas);
-		return obterTarefass(response.data.objeto.tarefas);
-	}, function errorCallback(response){
-        console.log('ERRO');
-        console.log(response);
+	    $routeProvider
+		    .when("/", {
+		        templateUrl: "main.html",
+		        controller: 'mainController'
+		    })
+		    .when("/nova-tarefa", {
+		        templateUrl: "nova_tarefa.html",
+		        controller: 'mainController'
+		    });
 	});
 
-}
+	tcapp.controller('mainController', function($scope, $http, $location) {
 
 
-function verDiaOffset(diaOffset, $scope, $http){
-
-	$scope.tarefasDoDia = $http.post('/api/obterTarefas', {dia: addDays(diaInicial, diaOffset)}).then( function successCallback(response){
-			
-			diaInicial = addDays(diaInicial, diaOffset);
-			
-			$scope.diaAtual = { data: formatarDDMMYYYY(diaInicial) };
-			$scope.proximoDia = { data: formatarDDMMYYYY(addDays(diaInicial, 1)) };
-			$scope.diaAnterior = { data: formatarDDMMYYYY(addDays(diaInicial, -1)) };
-
-			return obterTarefass(response.data.objeto.tarefas);
-
-		}, function errorCallback(response){
-	        console.log('ERRO');
-	        console.log(response);
-		});
-
-}
-
-var diaInicial = new Date();
+		obterTarefasDoDia($scope, $http);
 
 
-
-function mainController($scope, $http) {
-
-	
-
-	obterTarefasDoDia($scope, $http);
-
-
-	
-	$scope.diaAtual = { data: formatarDDMMYYYY(diaInicial) };
-	
-	
-	
-	$scope.diaAnterior = { data: formatarDDMMYYYY(addDays(diaInicial, -1)) };
-	
-	
-	
-	$scope.proximoDia = { data: formatarDDMMYYYY(addDays(diaInicial,1)) };
-
-	
-
-	$scope.titulos = $http.get('/api/obterTitulos').then(function successCallback(response){
 		
-		return response.data.objeto.titulos;
+		$scope.diaAtual = { data: formatarDDMMYYYY(diaInicial) };
+		
+		
+		
+		$scope.diaAnterior = { data: formatarDDMMYYYY(addDays(diaInicial, -1)) };
+		
+		
+		
+		$scope.proximoDia = { data: formatarDDMMYYYY(addDays(diaInicial,1)) };
+
+		
+
+		$http.get('/api/obterTitulos')
+			.then(function(response){
 				
-	}, function errorCallback(response){
-        console.log('ERRO');
-        console.log(response);
-	});
+				$scope.titulos =  response.data.objeto.titulos;
+						
+			}, function error(response){
+		        console.log('ERRO');
+		        console.log(response);
+			});
 
 
 
-	$scope.recorrencias = $http.get('/api/obterRecorrencias').then(function successCallback(response){
+		$http.get('/api/obterRecorrencias')
+			.then(function(response){
 
-		return response.data.objeto.recorrencias;
+				$scope.recorrencias = response.data.objeto.recorrencias;
 
-	}, function errorCallback(response){
-        console.log('ERRO');
-        console.log(response);
-	});
-
-
-
-	$scope.novaTarefa = function() {
-
-		$scope.novaTarefaEscondida = !$scope.novaTarefaEscondida;
-	}
-	
-	
-
-	$scope.home = function() {
-
-		if (!$scope.novaTarefaEscondida) {
-			$scope.novaTarefaEscondida = true;
-		}
-	}
+			}, function errorCallback(response){
+		        console.log('ERRO');
+		        console.log(response);
+			});
 
 
-
-	$scope.comecos = [ { id: '1', descricao:'Hoje' }, { id: 2, descricao: 'Amanhã' }, { id: 3, descricao: 'Depois de Amanhã' }];
-	
-	
-	
-	$scope.atualizaInicio = function(){
 
 		$scope.comecos = [ { id: '1', descricao:'Hoje' }, { id: 2, descricao: 'Amanhã' }, { id: 3, descricao: 'Depois de Amanhã' }];
-	}
+		
+		
+		
+		$scope.atualizaInicio = function(){
+
+			$scope.comecos = [ { id: '1', descricao:'Hoje' }, { id: 2, descricao: 'Amanhã' }, { id: 3, descricao: 'Depois de Amanhã' }];
+		}
 
 
 
-	$scope.gravarNovaConfiguracao = function(configuracao){
+		$scope.gravarNovaConfiguracao = function(configuracao){
 
-        $http.post('/api/gravarNovaConfiguracao', configuracao)
-            .success(function(data) {
-            	$scope.novaTarefaEscondida = true;
-                $scope.configuracao = {}; 
-                obterTarefasDoDia($scope, $http);
-            })
-            .error(function(data) {
-                console.log('Error: ' + data.mensagem);
-            });
-	}
-	
-	
-
-	$scope.verProximoDia = function(){ 
-
-		verDiaOffset(1, $scope, $http);
-	}
-	
-	
-
-	$scope.verDiaAnterior = function(){ 
-
-		verDiaOffset(-1, $scope, $http);
-	}
-	
-	
-
-	$scope.concluirTarefa = function(tarefa) {
-
-		if (tarefa.concluida != null && tarefa.concluida == false) {
-
-			tarefa.concluida = true;
-			tarefa.estilo = estiloTarefaConcluida;
-
-	        $http
-		        .post('/api/concluirTarefa', {
-		        	configuracaoId: tarefa.configuracaoId, 
-		        	data: tarefa.data,
-		        	concluir: true
-		        })
+	        $http.post('/api/gravarNovaConfiguracao', configuracao)
 	            .success(function(data) {
-	                $scope.sistema = {}; 
-	                $scope.sistema.resultadocriacao = data.mensagem;
-					$scope.sistemas.push(data.objeto.sistema); 
-	            })
-	            .error(function(data) {
-	                console.log('Error: ' + data.mensagem);
-	            });
-		} else if (tarefa.concluida != null && tarefa.concluida == true) {
-			tarefa.concluida = false;
-			tarefa.estilo = estiloTarefaNaoConcluida;
-
-	        $http
-		        .post('/api/concluirTarefa', {
-		        	configuracaoId: tarefa.configuracaoId, 
-		        	data: tarefa.data,
-		        	concluir: false
-		        })
-	            .success(function(data) {
-	                $scope.sistema = {}; 
-	                $scope.sistema.resultadocriacao = data.mensagem;
-					$scope.sistemas.push(data.objeto.sistema); 
+	                $scope.configuracao = {}; 
+	                obterTarefasDoDia($scope, $http);
+	                $location.path('/');
 	            })
 	            .error(function(data) {
 	                console.log('Error: ' + data.mensagem);
 	            });
 		}
+		
+		
+
+		$scope.verProximoDia = function(){ 
+
+			verDiaOffset(1, $scope, $http);
+		}
+		
+		
+
+		$scope.verDiaAnterior = function(){ 
+
+			verDiaOffset(-1, $scope, $http);
+		}
+		
+		
+
+		$scope.concluirTarefa = function(tarefa) {
+
+			if (tarefa.concluida != null && tarefa.concluida == false) {
+
+				tarefa.concluida = true;
+				tarefa.estilo = estiloTarefaConcluida;
+
+		        $http
+			        .post('/api/concluirTarefa', {
+			        	configuracaoId: tarefa.configuracaoId, 
+			        	data: tarefa.data,
+			        	concluir: true
+			        })
+		            .success(function(data) {
+		                $scope.sistema = {}; 
+		                $scope.sistema.resultadocriacao = data.mensagem;
+						$scope.sistemas.push(data.objeto.sistema); 
+		            })
+		            .error(function(data) {
+		                console.log('Error: ' + data.mensagem);
+		            });
+			} else if (tarefa.concluida != null && tarefa.concluida == true) {
+				tarefa.concluida = false;
+				tarefa.estilo = estiloTarefaNaoConcluida;
+
+		        $http
+			        .post('/api/concluirTarefa', {
+			        	configuracaoId: tarefa.configuracaoId, 
+			        	data: tarefa.data,
+			        	concluir: false
+			        })
+		            .success(function(data) {
+		                $scope.sistema = {}; 
+		                $scope.sistema.resultadocriacao = data.mensagem;
+						$scope.sistemas.push(data.objeto.sistema); 
+		            })
+		            .error(function(data) {
+		                console.log('Error: ' + data.mensagem);
+		            });
+			}
+		}		
+	});
+
+	var diaInicial = new Date();
+
+	function obterTarefasDoDia($scope, $http){
+
+		$http.post('/api/obterTarefas', { dia: diaInicial })
+			.then( function(response){
+				
+				console.log(response.data.objeto.tarefas);
+				$scope.tarefasDoDia = obterTarefass(response.data.objeto.tarefas);
+			}, function error(response){
+		        console.log('ERRO');
+		        console.log(response);
+			});
+	}
+
+	var estiloTarefaConcluida = {
+				'background-color': '#CCCCCC',
+				'color': '#EEEEEE !important',
+				'text-decoration': 'line-through'
+			};
+	var estiloTarefaNaoConcluida = {
+				'background-color': '#FFF',
+				'color': 'rgba(0,0,0,0.87)',
+				'text-decoration': 'none'
+			};
+
+	function formatarDDMMYYYY(dateFieldValue){
+
+		var year = dateFieldValue.getFullYear()+"";
+		var month = (dateFieldValue.getMonth()+1)+"";
+		var day = dateFieldValue.getDate()+"";
+		var dateFormat = day + "/" + month + "/" + year;
+
+		return dateFormat;
+	}
+
+	function formatarDDMMYYYYHHMM(data){
+
+		var dateFieldValue = new Date();
+		var dd = dateFieldValue.getDate() + "";
+		var mo = (dateFieldValue.getMonth() + 1) + "";
+		var yyyy = dateFieldValue.getFullYear() + "";
+		var hh = dateFieldValue.getHours() + "";
+		var mm = dateFieldValue.getMinutes() + "";
+		var dateFormat = dd + "/" + mo + "/" + yyyy + " " + hh + "h" + mm;
+
+		return dateFormat;
+	}
+
+	function obterTarefas($scope, $http){
+		
+		$http.get('/api/obterTarefas').success(function(data){
+
+			//console.log('data');console.log(data);console.log('');
+
+			//$scope.tarefas = data.objeto.tarefas;
+		}).error(function(data){
+	        console.log('');
+		});
+	}
+
+	function addDays(date, days) {
+	    var result = new Date(date);
+	    result.setDate(result.getDate() + days);
+	    return result;
+	}
+
+	function obterTarefass(d){
+
+		var tarefas = [];
+		var tarefasDb = d;
+
+		var qtd = tarefasDb.length;
+
+		for(i = 0; i < qtd; i++){
+			if (tarefasDb[i].concluida == true) {
+				tarefasDb[i].estilo = estiloTarefaConcluida;
+			} else if (tarefasDb[i].concluida == false) {
+				tarefasDb[i].estilo = estiloTarefaNaoConcluida;
+			} 
+			tarefas.push(tarefasDb[i]);
+		}
+
+		return tarefas;
+	}
+
+	function verDiaOffset(diaOffset, $scope, $http){
+
+		$http.post('/api/obterTarefas', {dia: addDays(diaInicial, diaOffset)})
+			.then(function(response){
+				
+				diaInicial = addDays(diaInicial, diaOffset);
+				
+				$scope.diaAtual = { data: formatarDDMMYYYY(diaInicial) };
+				$scope.proximoDia = { data: formatarDDMMYYYY(addDays(diaInicial, 1)) };
+				$scope.diaAnterior = { data: formatarDDMMYYYY(addDays(diaInicial, -1)) };
+
+				$scope.tarefasDoDia = obterTarefass(response.data.objeto.tarefas);
+
+			}, function error(response){
+		        console.log('ERRO');
+		        console.log(response);
+			});
 	}
 	
-	
-	
-}
+
+})();
